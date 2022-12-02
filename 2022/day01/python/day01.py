750fe1443766d55348d48473bdd50174c5df7736
Kirk Hofer <khofer@gmail.com>
2022-12-02 03:58:03 -0600

    update python

--- 2022/day01/python/day01.py
+++ 2022/day01/python/day01.py
@@ -1,6 +1,6 @@
 #From Nicole using map to do the leg work
-print(max(sum(map(int,x.split()))for x in open('2022/day1/input.txt').read().split('\n\n')))
-print(sum(sorted(sum(map(int,x.split()))for x in open('2022/day1/input.txt').read().split('\n\n'))[-3:]))
+print(max(sum(map(int,x.split()))for x in open('2022/day01/input.txt').read().split('\n\n')))
+print(sum(sorted(sum(map(int,x.split()))for x in open('2022/day01/input.txt').read().split('\n\n'))[-3:]))
 
 # for line in open('2022/day1/input.txt',).read().split('\n\n'):
 #     print(line)
