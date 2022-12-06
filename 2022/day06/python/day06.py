c9a4391047d4aab18c60245a48118f4383bba17f
Kirk Hofer <khofer@gmail.com>
2022-12-05 23:47:23 -0600

    list comp

--- 2022/day06/python/day06.py
+++ 2022/day06/python/day06.py
@@ -1,11 +1,23 @@
 '''
+I did do this in PowerShell first ;)
+
 x=4 or 14
 Look back to find the first place the last `x` characters aren't repeated plus 1
 '''
 b=open("../input.txt").read()
 
-for x in [4,14]:
+print("Pattern A: Just make it work")
+#long way
+for i,x in enumerate([4,14]):
     for r in range(x,len(b)):
         if(len(set(b[r-x:r]))==x):
-            print(x,' is ',r)
+            print("Answer {}={}".format(i+1,r))
             break
+
+print("Pattern B: List Comprehension")
+#Let's try this in list comprehension
+l = [[r for r in range(x,len(b)) if(len(set(b[r-x:r]))==x)][0] for x in [4,14]]
+print("Answer 1={}".format(l[0]))
+print("Answer 2={}".format(l[1]))
+
+#Whoa, I got inline with Nicole's thinking finally 
